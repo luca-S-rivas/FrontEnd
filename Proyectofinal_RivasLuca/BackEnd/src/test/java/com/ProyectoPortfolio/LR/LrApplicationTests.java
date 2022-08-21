97f1d9a8533eff12c21b9285585fd7c0135f48ed
luca-S-rivas <luca.rivas1992@gmail.com>
2022-08-21 18:11:51 -0300

    creacion de carpeta
    creacion de carpeta frontend para mas estar mas organizado

---
+++ Proyectofinal_RivasLuca/BackEnd/src/test/java/com/ProyectoPortfolio/LR/LrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ProyectoPortfolio.LR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
